337635e65128d63d5d9e43a505c992484296924f
forrany <gaoyaqi411@126.com>
2025-11-24 16:22:31 +0800

    feat: add support for inline SCSS and CSS module declarations in TypeScript

--- packages/core/src/vite-env.d.ts
+++ packages/core/src/vite-env.d.ts
@@ -4,6 +4,11 @@
  * SCSS 模块类型声明
  * 允许在 TypeScript 中导入 .scss 文件
  */
+declare module '*.scss?inline' {
+  const content: string;
+  export default content;
+}
+
 declare module '*.scss' {
   const content: string;
   export default content;
---
+++ packages/playground/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+declare module '*.scss?inline' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.css?inline' {
+  const content: string;
+  export default content;
+}
+
